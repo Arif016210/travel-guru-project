4217e2e84ce69f5866705ba9292e52cef6019b41
Arif016210 <arifulislamwasim2017@gmail.com>
2021-02-08 03:12:19 +0600

    add router dom

---
+++ src/components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <section>
+            <h2>Page Not Found, 404!!!</h2>
+        </section>
+    );
+};
+
+export default Error;
