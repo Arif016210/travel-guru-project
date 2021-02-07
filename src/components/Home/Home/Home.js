a497c2eb5754f4fee9f6ed6c0a6fc8159bad32c2
Arif016210 <arifulislamwasim2017@gmail.com>
2021-02-08 05:14:15 +0600

    background & Navbar done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Home.css';
 import Header from '../../Shared/Header/Header';
 import Slider from '../Slider/Slider';
 
 const Home = () => {
     return (
-        <section>
+        <section className="home">
             <Header></Header>
             <h2> This is Home page!!!</h2>
             <Slider></Slider>
